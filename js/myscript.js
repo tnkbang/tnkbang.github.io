7db359f985109982f19ea04ee232923c466997a4
Khánh Băng <104491826+tnkbang@users.noreply.github.com>
2022-08-12 16:15:24 +0700

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+﻿
+$(document).ready(function () {
+    $('#thongbao').removeClass('hide');
+    $('#thongbao').delay(2000).slideUp(1000);
+});
+
+function animation(){
+    document.getElementById('tieude').style.animation='hieuung4 2000ms infinite';
+}
+function noanimation(){
+    document.getElementById('tieude').style.animation='none';
+}
